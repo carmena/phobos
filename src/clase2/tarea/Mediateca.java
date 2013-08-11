4e3ca6bd163fef25065c88f4db81d23531a2fdbc
carmena <carmenasencio_2206@hotmail.com>
2013-08-11 14:28:03 -0500

    Update Mediateca.java

--- src/clase2/tarea/Mediateca.java
+++ src/clase2/tarea/Mediateca.java
@@ -1,13 +1,6 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package clase2.tarea;
 
-/**
- *
- * @author PFR
- */
 public class Mediateca {
     
 }
